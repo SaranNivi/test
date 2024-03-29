826ea73de8f31dbb63a411b2d4381122fa92dd0b
EC2 Default User <ec2-user@ip-172-31-35-244.eu-north-1.compute.internal>
2024-03-29 11:07:18 +0000

    script file

---
+++ gitbranch.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+num1=4
+num2=7
+sum=$((num1 + num2))
+echo "the sum of $num1 and $num2 is: $sum"
+~                                                                                                                                                                                                                  
+~                                                                                                                                                                                                                  
+~                                             
